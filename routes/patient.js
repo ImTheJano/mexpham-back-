1e40ab5db3b9eca12335bb4b76c4b2d3c74c15e4
imthejano <imthejano@outlook.com>
2019-11-26 17:02:37 -0600

    routes

--- routes/patient.js
+++ routes/patient.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
-const {get,store,update,del}=require('../controllers/Patient');
+const {get,store,update,del,storeByGet}=require('../controllers/Patient');
 
 router.get('/:find',get);
 router.post('/',store);
 router.put('/:id',update);
 router.delete('/:id',del);
-router.get('post/:params',del);
+router.get('post/:params',storeByGet);
 
 
 module.exports=router;
